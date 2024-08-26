f58a0d32f1f8eed92c8fedbb530365fb3f4d196e
Punyapal Shah <mrpunyapal@gmail.com>
2024-08-26 18:14:00 +0530

    chore: Update SendReminderEmailsCommand to send reminder emails to users daily and weekly

--- routes/console.php
+++ routes/console.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 use App\Console\Commands\DeleteNonEmailVerifiedUsersCommand;
 use App\Console\Commands\PerformDatabaseBackupCommand;
-use App\Console\Commands\SendDailyEmailsCommand;
-use App\Console\Commands\SendWeeklyEmailsCommand;
+use App\Console\Commands\SendReminderEmailsCommand;
 use App\Console\Commands\SyncVerifiedUsersCommand;
 use App\Jobs\CleanUnusedUploadedImages;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(SendDailyEmailsCommand::class)->dailyAt('13:00');
-Schedule::command(SendWeeklyEmailsCommand::class)->weeklyOn(1, '13:00');
+Schedule::command(SendReminderEmailsCommand::class)->dailyAt('13:00');
+Schedule::command(SendReminderEmailsCommand::class, ['--weekly' => true])->weeklyOn(1, '13:00');
 Schedule::command(PerformDatabaseBackupCommand::class)->hourly();
 Schedule::command(DeleteNonEmailVerifiedUsersCommand::class)->hourly();
 Schedule::command(SyncVerifiedUsersCommand::class)->daily();
